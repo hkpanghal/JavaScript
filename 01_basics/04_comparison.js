b484d3ef0623d6382dd123fa45837b24aa732ecf
hkpanghal <112403425+hkpanghal@users.noreply.github.com>
2023-10-19 15:37:37 +0000

    successfully learnt the basic concepts about comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,80 @@
+// basic comparison
+// comparison in same datatypes
+
+// nubmber
+// console.log(1==2)
+// console.log(1>2)
+// console.log(1<2)
+// console.log(1>=2)
+// console.log(1<=2)
+
+// o/p ->
+// false
+// false
+// true
+// false
+// true
+
+// string
+// console.log("saurav" == "saurav")
+// console.log("saurav" > "saurav")
+// console.log("saurav" < "saurav")
+// console.log("saurav" >= "saurav")
+// console.log("saurav" <= "saurav")
+
+// o/p->
+// true
+// false
+// false
+// true
+// true
+
+// boolean
+
+// console.log(true == true)
+// console.log(true > true)
+// console.log(true < true)
+// console.log(true >= true)
+// console.log(true <= true)
+
+// o/p->
+// true
+// false
+// false
+// true
+// true
+
+
+// ********************************** comaprison in different datatypes ***************************
+
+// console.log(null > 0)
+// console.log(null < 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+// console.log(null <= 0)
+
+// o/p->
+// false
+// false
+// false
+// true
+// true
+
+
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+
+// o/p ->
+// false
+// false
+// false
+// false
+// false
+
+
+// note => javascript me == and === and (<,>,>=,<=) alg alg tarike se kam karte h
+
+
